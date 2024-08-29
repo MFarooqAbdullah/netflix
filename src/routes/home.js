1844ff62d60457275742eb4d123abc3243fb944f
Farooq <mr.farooq.ok@gmail.com>
2024-08-29 18:44:06 +0500

    change

--- src/routes/home.js
+++ src/routes/home.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 // Index home
 router.get('/', async (req, res) => {
-  res.json("welcom to home");
+  res.json("welcom to Homitag netflix genre/movies API");
 });
 
 module.exports = router;
